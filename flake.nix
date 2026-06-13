7c27eb3fac9c84983dc7935143cad48fd9b4686e
li <li@goldragon.criome.net>
2026-06-13 06:01:47 +0200

    mirror: add flake packaging

---
+++ flake.nix
@@ -0,0 +1,164 @@
+{
+  description = "mirror - Payload-blind append-ingest mirror daemon for sema version control.";
+
+  inputs = {
+    nixpkgs.url = "github:LiGoldragon/nixpkgs?ref=main";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      crane,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forSystems = function: nixpkgs.lib.genAttrs systems (system: function system);
+
+      mkContext =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          toolchain = fenix.packages.${system}.stable.withComponents [
+            "cargo"
+            "rustc"
+            "rustfmt"
+            "clippy"
+            "rust-src"
+          ];
+          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+          schemaFilter =
+            path: type:
+            (type == "regular" || type == "directory") && (builtins.match ".*/schema(/.*)?" path != null);
+          sourceFilter = path: type: (craneLib.filterCargoSources path type) || (schemaFilter path type);
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = sourceFilter;
+            name = "source";
+          };
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in
+        {
+          inherit
+            pkgs
+            toolchain
+            craneLib
+            src
+            commonArgs
+            cargoArtifacts
+            ;
+        };
+    in
+    {
+      packages = forSystems (
+        system:
+        let
+          context = mkContext system;
+        in
+        {
+          default = context.craneLib.buildPackage (
+            context.commonArgs
+            // {
+              inherit (context) cargoArtifacts;
+              pname = "mirror";
+              meta.mainProgram = "mirror-daemon";
+            }
+          );
+        }
+      );
+
+      checks = forSystems (
+        system:
+        let
+          context = mkContext system;
+        in
+        {
+          default = context.craneLib.cargoTest (
+            context.commonArgs
+            // {
+              inherit (context) cargoArtifacts;
+            }
+          );
+          build = context.craneLib.cargoBuild (
+            context.commonArgs
+            // {
+              inherit (context) cargoArtifacts;
+            }
+          );
+          test = context.craneLib.cargoTest (
+            context.commonArgs
+            // {
+              inherit (context) cargoArtifacts;
+            }
+          );
+          test-nota-text = context.craneLib.cargoTest (
+            context.commonArgs
+            // {
+              inherit (context) cargoArtifacts;
+              cargoTestExtraArgs = "--features nota-text --all-targets";
+            }
+          );
+          fmt = context.craneLib.cargoFmt {
+            inherit (context) src;
+          };
+          clippy = context.craneLib.cargoClippy (
+            context.commonArgs
+            // {
+              inherit (context) cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- -D warnings";
+            }
+          );
+        }
+      );
+
+      apps = forSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/mirror";
+        };
+        daemon = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/mirror-daemon";
+        };
+        meta = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/meta-mirror";
+        };
+        write-configuration = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/mirror-write-configuration";
+        };
+      });
+
+      formatter = forSystems (system: (mkContext system).pkgs.nixfmt-rfc-style);
+
+      devShells = forSystems (
+        system:
+        let
+          context = mkContext system;
+        in
+        {
+          default = context.pkgs.mkShell {
+            name = "mirror";
+            packages = [
+              context.pkgs.jujutsu
+              context.toolchain
+            ];
+          };
+        }
+      );
+    };
+}
